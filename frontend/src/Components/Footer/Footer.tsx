978237bff5587ccf5e1cf5cf1391bf7c03db95bf
nefot <arresrtd@m>
2025-10-13 17:04:54 +0300

    refractor

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,58 +1,55 @@
+import './Footer.scss';
+
 const footerLink = [
-    {name: "Специалистам", href: "/"},
-    {name: "Организациям", href: "/"},
-    {name: "Нормативно правовая база", href: "/"},
-    {name: "Новости", href: "/"},
-    {name: "Анонсы", href: "/"},
-    {name: "Специалистам", href: "/"},
-    {name: "Организациям", href: "/"},
-    {name: "Нормативно правовая база", href: "/"},
-    {name: "Новости", href: "/"},
-    {name: "Анонсы", href: "/"},
-    {name: "Специалистам", href: "/"},
-    {name: "Организациям", href: "/"},
-    {name: "Нормативно правовая база", href: "/"},
-    {name: "Новости", href: "/"},
-    {name: "Анонсы", href: "/"}
-]
+  { name: 'Специалистам', href: '/' },
+  { name: 'Организациям', href: '/' },
+  { name: 'Нормативно правовая база', href: '/' },
+  { name: 'Новости', href: '/' },
+  { name: 'Анонсы', href: '/' },
+  { name: 'Специалистам', href: '/' },
+  { name: 'Организациям', href: '/' },
+  { name: 'Нормативно правовая база', href: '/' },
+  { name: 'Новости', href: '/' },
+  { name: 'Анонсы', href: '/' },
+  { name: 'Специалистам', href: '/' },
+  { name: 'Организациям', href: '/' },
+  { name: 'Нормативно правовая база', href: '/' },
+  { name: 'Новости', href: '/' },
+  { name: 'Анонсы', href: '/' },
+];
 
 
 export default function Footer() {
-    return (
-        <>
-            <div className="footer">
-                <div className="footer-container">
-                    <h1>MedArchive</h1>
-                        {footerLink[0]["name"]}
-                        {footerLink[1]["name"]}
-                        {footerLink[2]["name"]}
-                        {footerLink[3]["name"]}
-                        {footerLink[4]["name"]}
-                    <div className="footer-section">
-                        {footerLink[0]["name"]}
-                        {footerLink[1]["name"]}
-                        {footerLink[2]["name"]}
-                        {footerLink[3]["name"]}
-                        {footerLink[4]["name"]}
-                    </div>
-                    <div className="footer-section">
-                        {footerLink[0]["name"]}
-                        {footerLink[1]["name"]}
-                        {footerLink[2]["name"]}
-                        {footerLink[3]["name"]}
-                        {footerLink[4]["name"]}
-                    </div>
-                    <div className="footer-section">
-                        {footerLink[0]["name"]}
-                        {footerLink[1]["name"]}
-                        {footerLink[2]["name"]}
-                        {footerLink[3]["name"]}
-                        {footerLink[4]["name"]}`
-                    </div>
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-container">
+          <h1>MedArchive</h1>
+          <div className="footer-section">
+            <div className=""><a>{footerLink[0]['name']}</a></div>
+            <div className=""><a>{footerLink[1]['name']}</a></div>
+            <div className=""><a>{footerLink[2]['name']}</a></div>
+            <div className=""><a>{footerLink[3]['name']}</a></div>
+            <div className=""><a>{footerLink[4]['name']}</a></div>
+          </div>
+          <div className="footer-section">
+            <a>{footerLink[0]['name']}</a>
+            <a>{footerLink[1]['name']}</a>
+            <a>{footerLink[2]['name']}</a>
+            <a>{footerLink[3]['name']}</a>
+            <a>{footerLink[4]['name']}</a>
+          </div>
+          <div className="footer-section">
+            <a>{footerLink[0]['name']}</a>
+            <a>{footerLink[1]['name']}</a>
+            <a>{footerLink[2]['name']}</a>
+            <a>{footerLink[3]['name']}</a>
+            <a>{footerLink[4]['name']}</a>
+          </div>
 
 
-                </div>
-            </div>
-        </>
-    );
+        </div>
+      </div>
+    </>
+  );
 }
